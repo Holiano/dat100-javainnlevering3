977ab5192c87dc06b8c96c07f038fa7e62ba9205
Holianox <bosdalj@gmail.com>
2024-11-11 13:49:31 +0100

    yeet
    eyt

--- src/main/java/no/hvl/dat100/oppgave3/Blogg.java
+++ src/main/java/no/hvl/dat100/oppgave3/Blogg.java
@@ -3,70 +3,123 @@ package no.hvl.dat100.oppgave3;
 import no.hvl.dat100.common.TODO;
 import no.hvl.dat100.oppgave1.*;
 
-public class Blogg {
+public class Blogg extends Innlegg {
 
-	// TODO: objektvariable 
+	private Innlegg[] innleggtabell;
+	private int nesteLedige;
 
 	public Blogg() {
-		throw new UnsupportedOperationException(TODO.constructor("Blogg"));
+		this.innleggtabell = new Innlegg[20];
+
 	}
 
 	public Blogg(int lengde) {
-		throw new UnsupportedOperationException(TODO.constructor("Blogg"));
+		this.innleggtabell = new Innlegg[lengde];
 	}
 
 	public int getAntall() {
-		throw new UnsupportedOperationException(TODO.method());
+		return nesteLedige;
+
 	}
-	
+
 	public Innlegg[] getSamling() {
-		throw new UnsupportedOperationException(TODO.method());
-
+		return this.innleggtabell;
 	}
-	
-	public int finnInnlegg(Innlegg innlegg) {
 
-		throw new UnsupportedOperationException(TODO.method());
+	public int finnInnlegg(Innlegg innlegg) {
+		for (int i = 0; i < innleggtabell.length; i++) {
+			if (innleggtabell[i] != null && innleggtabell[i].erLik(innlegg)) {
+				return i;
+			}
+		}
+		return -1;
 	}
 
 	public boolean finnes(Innlegg innlegg) {
-		throw new UnsupportedOperationException(TODO.method());
+		for (Innlegg i : innleggtabell) {
+			if (i != null && i.erLik(innlegg)) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	public boolean ledigPlass() {
-		throw new UnsupportedOperationException(TODO.method());
-
+		for (Innlegg i : innleggtabell) {
+			if (i == null) {
+				return true;
+			}
+		}
+		return false;
 	}
-	
+
 	public boolean leggTil(Innlegg innlegg) {
 
-		throw new UnsupportedOperationException(TODO.method());
+		if (!finnes(innlegg) && ledigPlass()) {
+			innleggtabell[getAntall()] = innlegg;
+			nesteLedige++;
+			return true;
+		}
+
+		return false;
 	}
-	
+
+	@Override
 	public String toString() {
-		throw new UnsupportedOperationException(TODO.method());
+		StringBuilder sb = new StringBuilder();
+		sb.append(getAntall()).append("\n");
+		for (Innlegg i : innleggtabell) {
+			if (i != null) {
+				sb.append(i.toString());
+			}
+		}
+		return sb.toString();
 	}
 
-	// valgfrie oppgaver nedenfor
-	
 	public void utvid() {
-		throw new UnsupportedOperationException(TODO.method());
+		Innlegg[] tab = new Innlegg[innleggtabell.length * 2];
+		for (int i = 0; i < innleggtabell.length; i++) {
+			tab[i] = innleggtabell[i];
+		}
+		innleggtabell = tab;
 	}
-	
+
 	public boolean leggTilUtvid(Innlegg innlegg) {
+		for (int i = 0; i < innleggtabell.length; i++) {
+			if (innleggtabell[i] != null && innleggtabell[i].getId() == innlegg.getId()) {
+				return false;
+			}
+		}
+		if (innleggtabell.length == nesteLedige) {
+			utvid();
+		}
+		innleggtabell[nesteLedige] = innlegg;
+		nesteLedige++;
+		return true;
 
-		throw new UnsupportedOperationException(TODO.method());
-		
 	}
-	
+
 	public boolean slett(Innlegg innlegg) {
-		
-		throw new UnsupportedOperationException(TODO.method());
+		for (int i = 0; i < nesteLedige; i++) {
+			if (innleggtabell[i].getId() == innlegg.getId()) {
+				innleggtabell[i] = innleggtabell[nesteLedige - 1];
+				innleggtabell[nesteLedige - 1] = null;
+				nesteLedige--;
+				return true;
+			}
+		}
+		return false;
 	}
-	
-	public int[] search(String keyword) {
-		
-		throw new UnsupportedOperationException(TODO.method());
 
+	public int[] search(String keyword) {
+		int[] tab = new int[innleggtabell.length];
+		int x = 0;
+		for (int i = 0; i < innleggtabell.length; i++) {
+			if (innleggtabell[i].toString().contains(keyword)) {
+				tab[x] = innleggtabell[i].getId();
+				x++;
+			}
+		}
+		return tab;
 	}
 }
