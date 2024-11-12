8e4fedfeb6c5f5ebbf15ffa61d8db43d32729462
Holianox <bosdalj@gmail.com>
2024-11-12 19:35:53 +0100

    oppdatert kode
    ye

--- src/main/java/no/hvl/dat100/oppgave4/SkrivBlogg.java
+++ src/main/java/no/hvl/dat100/oppgave4/SkrivBlogg.java
@@ -1,15 +1,40 @@
 package no.hvl.dat100.oppgave4;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.PrintWriter;
+import java.io.FileReader;
+import java.io.IOException;
 
-import no.hvl.dat100.common.TODO;
-import no.hvl.dat100.oppgave3.*;
+import no.hvl.dat100.oppgave3.Blogg;
 
 public class SkrivBlogg {
 
 	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
-
-		throw new UnsupportedOperationException(TODO.method());
+try {
+		File file = new File(mappe + filnavn);
+		if (!file.exists()) {
+			throw new FileNotFoundException(filnavn + "Ikkje funnet");
+		}
+		
+		FileReader fr = new FileReader(file);
+		
+		BufferedReader bf = new BufferedReader(fr);
+		
+		String line;
+		while((line = bf.readLine()) != null) {
+			System.out.println(line);
+		}
+		bf.close();
+		return true;
+} catch (FileNotFoundException e) {
+	System.out.println("Kan ikkje finne filen");
+	e.printStackTrace();
+	return false;
+} catch (IOException e) {
+	System.out.println("Det skjedde ein feil med lesing av fil");
+	e.printStackTrace();
+	return false;
+}
 	}
 }
